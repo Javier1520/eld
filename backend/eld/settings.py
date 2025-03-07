0e18b6964957aa62dd4f594043cc8091951d0590
Javier Guerrero <jeguerrerocordoba@gmail.com>
2025-03-07 12:52:56 -0500

    add env

--- backend/eld/settings.py
+++ backend/eld/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m(zb7n@wb45cc1^8lrky)q9vhd^c34$_xm5$rq9y22qzkqw&+f'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-m(zb7n@wb45cc1^8lrky)q9vhd^c34$_xm5$rq9y22qzkqw&+f')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
     'trips',
 ]
