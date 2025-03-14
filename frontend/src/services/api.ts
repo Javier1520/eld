12d99e472dd16c309b919c3c569dcb2b0f80db90
Javier Guerrero <jeguerrerocordoba@gmail.com>
2025-03-13 22:44:39 -0500

    env for deploy

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -8,7 +8,7 @@ import {
   LogFormData,
 } from "@/types";
 
-const API_URL = "http://localhost:8000/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Create axios instance
 const api = axios.create({
